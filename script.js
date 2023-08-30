99e27a4f66ec4867f947fb940bc7c3c3da9f5fa3
PSum <sum.philipp@gmail.com>
2023-08-30 18:46:34 +0200

    asdf

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const container = document.querySelector(".sketchArea");
-let etchSize = 32;
-container.style.gridTemplateColumns = `repeat(${etchSize}, 1fr)`
-container.style.gridTemplateRows = `repeat(${etchSize}, 1fr)`
+// const container = document.querySelector(".sketchArea");
+// let etchSize = 32;
+// container.style.gridTemplateColumns = `repeat(${etchSize}, 1fr)`
+// container.style.gridTemplateRows = `repeat(${etchSize}, 1fr)`
 
 
-function createArea (gridSize) {
-    let arraySize = gridSize * gridSize;
-    for (let i = 0; i < arraySize; i++){
-        let box = document.createElement("div");
-        // box.style.backgroundColor = "blue";
-        box.className = "sketchBox";
-        container.appendChild(box);
-    }
-}
+// function createArea (gridSize) {
+//     let arraySize = gridSize * gridSize;
+//     for (let i = 0; i < arraySize; i++){
+//         let box = document.createElement("div");
+//         // box.style.backgroundColor = "blue";
+//         box.className = "sketchBox";
+//         container.appendChild(box);
+//     }
+// }
 
-createArea(etchSize);
+// createArea(etchSize);
 
