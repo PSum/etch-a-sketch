d081158f383882068b2efbaa2d9931c2bf3353d8
Philipp <50810270+PSum@users.noreply.github.com>
2023-09-16 21:02:47 +0200

    basic working condition

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".sketchArea");
 let etchSize = 32;
+let isMouseDown = false;
+colorGlobal = "black";
 container.style.gridTemplateColumns = `repeat(${etchSize}, 1fr)`
 container.style.gridTemplateRows = `repeat(${etchSize}, 1fr)`
 
@@ -14,5 +16,48 @@ function createArea (gridSize) {
     }
 }
 
+function changeColor(event) {
+    if (isMouseDown == true){
+    event.target.style.backgroundColor = colorGlobal;
+    } 
+}
+
 createArea(etchSize);
+
+let divItems = document.querySelectorAll(".sketchBox");
+let greyButton = document.querySelector("#buttonGrey");
+let whiteButton = document.querySelector("#buttonWhite");
+let eraseButton = document.querySelector("#buttonErase");
+let blackButton = document.querySelector("#buttonBlack");
+
+blackButton.addEventListener("click", function () {
+    colorGlobal = "black";
+})
+
+greyButton.addEventListener("click", function () {
+    colorGlobal = "grey";
+})
+
+whiteButton.addEventListener("click", function () {
+    colorGlobal = "white";
+})
+
+// Add a mousedown event listener to set the isMouseDown flag to true
+document.addEventListener('mousedown', () => {
+    isMouseDown = true;
+});
+
+// Add a mouseup event listener to set the isMouseDown flag to false
+document.addEventListener('mouseup', () => {
+    isMouseDown = false;
+});
+
+
+divItems.forEach(function (div){
+    div.addEventListener('mouseover', changeColor);
+    eraseButton.addEventListener("click", function () {
+        div.style.backgroundColor = "white";
+    })
+})
+
 
